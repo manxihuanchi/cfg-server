472ef9e2367708830cb01ffc846d22d5b1248545
manxihuanchi <546568501@qq.com>
2020-04-13 10:59:43 +0800

    11

--- src/main/java/cho/carbon/cfg/mq/SendMQListener.java
+++ src/main/java/cho/carbon/cfg/mq/SendMQListener.java
@@ -1,16 +1,20 @@
 package cho.carbon.cfg.mq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
 
 import cho.carbon.utils.MessageDTO;
 
 
 public class SendMQListener implements ApplicationListener<ContextRefreshedEvent>{
 
+	 Logger   logger = LoggerFactory.getLogger(SendMQListener.class);
+	
+	
 	@Autowired
 	RabbitTemplate rabbitTemplate;
 	
@@ -21,7 +25,7 @@ public class SendMQListener implements ApplicationListener<ContextRefreshedEvent
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		// 容器刷新完成的时候， 发消息给消息中间件
 		rabbitTemplate.convertAndSend("carbon.fg", "carbonqueukey", message);
-		System.out.println("容器完成： " + event.toString());
+		logger.debug("容器完成： " + event.toString());
 	}
 
 }
